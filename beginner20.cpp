5c9adc23465100b7bd5f0ebc47bb6bc7b63776c3
Surbhi1104 <asurbhi124@gmail.com>
2019-01-22 22:57:29 +0530

    twentieth program

---
+++ beginner20.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main() {
+	     
+	    int a;
+		scanf("%d",&a);
+		for(int i=1;i<=5;++i)
+		{
+			printf("%d ",i*a);
+		}
+		 
+		return 0;
+}
