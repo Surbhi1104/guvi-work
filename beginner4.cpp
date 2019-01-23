262eddb87b809e2f0f4443ab9f8391e614ad6e5f
Surbhi1104 <asurbhi124@gmail.com>
2019-01-23 21:26:28 +0530

    fourth program

---
+++ beginner4.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	char ch;
+	scanf("%c",&ch);
+	if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+	printf("Alphabet\n");
+	else
+	printf("No\n");
+		return 0;
+}
